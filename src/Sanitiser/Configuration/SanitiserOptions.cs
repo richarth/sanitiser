a6392551b610b08c06198346a0f1dd1efa3c61b9
Mario Lopez <lopezadeh@gmail.com>
2025-11-12 11:09:27 +1100

    added UsersSanitiser

--- src/Sanitiser/Configuration/SanitiserOptions.cs
+++ src/Sanitiser/Configuration/SanitiserOptions.cs
@@ -5,4 +5,5 @@ public class SanitiserOptions
     public const string SanitiserOptionsKey = "Sanitiser";
     public bool Enable { get; init; }
     public MembersSanitiserOptions? MembersSanitiser { get; init; }
+    public UsersSanitiserOptions UsersSanitiser { get; init; } = new UsersSanitiserOptions();
 }
